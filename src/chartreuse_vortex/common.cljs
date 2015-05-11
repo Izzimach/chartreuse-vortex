5c525b67b0b200d0c72cce7301f6a3bf3a87b0d5
Gary Haussmann <gjhaussmann@gmail.com>
2015-05-11 14:19:38 -0600

    Allow downloading of timing data as EDN

--- src/chartreuse_vortex/common.cljs
+++ src/chartreuse_vortex/common.cljs
@@ -4,13 +4,10 @@
 
 (ns chartreuse-vortex.common
   (:require-macros [cljs.core.async.macros :refer [go]])
-
   (:require [om.core :as om :include-macros true]
-            [om-tools.core :as omtools :refer-macros [defcomponentk] :include-macros true]
+            [om-tools.core :refer-macros [defcomponentk] :include-macros true]
             [omreactpixi.abbrev :as pixi]
-            [cljs.core.async :as async :refer [>! <! put! chan]]
-            [schema.core :as schema]
-            [clojure.string :as string]))
+            [cljs.core.async :refer [>! <! put! chan]]))
 
 (defn assetpath-for [name] (str "../assets/" name))
 
@@ -38,11 +35,13 @@
 (defn shrinkbyspritesize [{:keys [width height]}]
   [(- width *spritesize*) (- height *spritesize*)])
 
+;;
+;; draws a single sprite. when clicked, it sends 'clickdata' over the specified channel
+;;
 (defcomponentk clickable-sprite [[:data x y image clickchannel clickdata] owner]
   (display-name [_] "ClickableSprite")
   (render [_] (let [clickfeed #(put! clickchannel clickdata)]
                 (pixi/sprite {:x x :y y :interactive true :image image :anchor *spritecenter* :click clickfeed}))))
-
 
 (defcomponentk spritecountlabel [[:data x y spritecount] owner]
   (display-name [_] "SpriteCountLabel")
@@ -81,44 +80,52 @@
       (pixi/displayobjectcontainer
         {:x x :y y}
         (om/build clickable-sprite {:x 20 :y 20 :key "startstop" :image startstopsprite :clickchannel clickchannel :clickdata "startstop"})
-        (om/build clickable-sprite {:x 60 :y 20 :key "cleardata" :image (assetpath-for "trashcan.png") :clickchannel clickchannel :clickdata "cleardata"})
-        (om/build clickable-sprite {:x 100 :y 20 :key "download" :image (assetpath-for "import.png") :clickchannel clickchannel :clickdata "download"})))))
+        (om/build clickable-sprite {:x 55 :y 20 :key "cleardata" :image (assetpath-for "trashcan.png") :clickchannel clickchannel :clickdata "cleardata"})
+        (om/build clickable-sprite {:x 90 :y 20 :key "download" :image (assetpath-for "import.png") :clickchannel clickchannel :clickdata "download"})))))
 
+;;
+;; given a cljs data structure convert to edn and "download" it
+;;
+
+(defn download-as-edn [data]
+  (let [datablob (js/Blob. #js [(str data)] #js {:type "application/text"})
+        url (.createObjectURL js/URL datablob)]
+    (.open js/window url)))
 ;;
 ;; handles clicking of the timing icons to start and stop recording of timing data and downloading it
 ;;
-(defcomponentk timing-controls [[:data x y spritecount] owner]
+(defcomponentk timing-controls [[:data x y sprites] owner]
   (display-name [_] "TimingControls")
   (init-state [_] {:recording? false :timingdata [] :clickchannel (chan 1) :rendertime 0 :updatetime 0})
   (did-mount [_]
     (let [clickchannel (om/get-state owner :clickchannel)
-          startstoprecording (fn []
+          start-stop-recording (fn []
                                (let [recording?        (om/get-state owner :recording?)
                                      newrecordingstate (not recording?)]
                                  (om/set-state! owner :recording? newrecordingstate)))
-          appendtimingdata (fn []
+          appendto-timing-data (fn []
                              (let [{:keys [rendertime updatetime timingdata]} (om/get-state owner)
-                                   newtimingdata                              (conj timingdata [spritecount rendertime updatetime])]
+                                   newtimingdata                              (conj timingdata [(count (:sprites (om/get-props owner))) rendertime updatetime])]
                                (om/set-state! owner :timingdata newtimingdata)))
-          updatefn (fn updatecallback [_]
+          get-timing-data (fn updatecallback [_]
                      (let [{:keys [rendertime updatetime]} @timingdata]
                        (om/set-state! owner :rendertime rendertime)
                        (om/set-state! owner :updatetime updatetime)
                        (if (om/get-state owner :recording?)
-                         (appendtimingdata))))]
+                         (appendto-timing-data))))]
       ;; process clicks on the various icons
       (go (loop [clickdata (<! clickchannel)]
             (.warn js/console clickdata)
             (cond
-              (= clickdata "startstop") (startstoprecording)
+              (= clickdata "startstop") (start-stop-recording)
               (= clickdata "cleardata") (om/set-state! owner :timingdata [])
-              (= clickdata "download") nil
+              (= clickdata "download") (download-as-edn (om/get-state owner :timingdata))
               :else (.warn js/console (str "Invalid data came through click channel:" clickdata)))
             (recur (<! clickchannel))))
       ;;
       ;; this auto-updates the data values at a set interval by pulling them out of the 'timingdata' atom
       ;;
-      (om/set-state! owner :updatecallback (js/setInterval updatefn 500))))
+      (om/set-state! owner :updatecallback (js/setInterval get-timing-data 500))))
   (will-unmount [_]
     ;; need to clean up the auto-updating
     (when-let [updatefn (om/get-state owner :updatecalback)]
@@ -129,15 +136,17 @@
           timingsamplecount                          (count timingdata)]
       (pixi/displayobjectcontainer
         {:x x :y y}
-        (om/build timinglabels (assoc commonprops :x 0 :y 0 :render-time rendertime :update-time updatetime :spritecount spritecount))
-        (om/build timing-icons (assoc commonprops :x 0 :y 24))
+        (om/build timinglabels (assoc commonprops :x 0 :y 0 :render-time rendertime :update-time updatetime :spritecount (count sprites)))
+        (om/build timing-icons (assoc commonprops :x -5 :y 24))
         (pixi/bitmaptext {:x 0 :y 65 :style *defaulttextstyle* :tint *defaulttexttint* :text (str timingsamplecount " samples collected")})))))
 
-(defcomponentk controlpanel [[:data x y spritecount] owner]
+(defcomponentk controlpanel [[:data x y sprites spritecontrolchannel] owner]
   (display-name [_] "TimingLabels")
   (render [_]
     (pixi/displayobjectcontainer
       {:x x :y y}
       (pixi/sprite {:image (assetpath-for "blue_panel.png") :scale *panelscale*})
-      (om/build spritecountlabel {:x 10 :y 5 :spritecount spritecount})
-      (om/build timing-controls {:x 10 :y 16 :spritecount spritecount}))))
+      (om/build spritecountlabel {:x 10 :y 5 :spritecount (count sprites)})
+      (om/build timing-controls {:x 10 :y 16 :sprites sprites})
+      (om/build clickable-sprite {:x 130 :y 60 :image (assetpath-for "plus.png") :clickchannel spritecontrolchannel :clickdata "plus"})
+      (om/build clickable-sprite {:x 170 :y 60 :image (assetpath-for "minus.png") :clickchannel spritecontrolchannel :clickdata "minus"}))))
